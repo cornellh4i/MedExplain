2f316506173a20f0aee31c32dca98f7b2e6036a0
brandon-lerit <brandon338@icloud.com>
2025-10-15 01:19:35 -0400

    Fix babe config

--- client/babel.config.js
+++ client/babel.config.js
@@ -1,4 +1,6 @@
-module.exports = {
-  presets: ["module:metro-react-native-babel-preset"],
-  plugins: ["tailwindcss-react-native/babel"],
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ["babel-preset-expo"],
+  };
 };
