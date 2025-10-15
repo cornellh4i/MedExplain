300c4dd0c76325bb56bcf04fcc7d382e1aa0a2ac
Jeremy Cortez <jeremycortez@Jeremys-MacBook-Pro.local>
2025-10-15 01:36:51 -0400

    jac722-Onboarding changes

--- client/babel.config.js
+++ client/babel.config.js
@@ -1,4 +1,12 @@
-module.exports = {
-  presets: ["module:metro-react-native-babel-preset"],
-  plugins: ["tailwindcss-react-native/babel"],
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: [
+      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+      "nativewind/babel",
+    ],
+    plugins: [
+      "react-native-reanimated/plugin",
+    ],
+  };
 };
