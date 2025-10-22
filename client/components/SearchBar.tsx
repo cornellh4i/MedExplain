296925126d74993789788326ef0b3ecb11c430c4
Jeremy Cortez <jeremycortez@Jeremys-MacBook-Pro.local>
2025-10-22 00:49:37 -0400

    Implemented revision to searchBar file  as noted and stated in feedback.

--- client/components/SearchBar.tsx
+++ client/components/SearchBar.tsx
@@ -1,41 +1,16 @@
-import * as React from "react";
-import { TextInput, TextInputProps, View, StyleSheet } from "react-native";
+import React, { forwardRef } from "react";
+import { TextInput, TextInputProps, View} from "react-native";
 
-type Props = TextInputProps & {
-  containerStyle?: object;
-  iconSize?: number;
-};
 
-const SearchBar = React.forwardRef<TextInput, Props>(
-  ({ iconSize = 16, className, ...props }, ref) => {
+const SearchBar = forwardRef<TextInput, TextInputProps>(
+  ({ className, ...props }, ref) => {
     return (
       <View>
-        <TextInput ref={ref} placeholderTextColor="#6b7280" {...props} />
+        <TextInput className={`border-2 border-gray-300 w-4/5 rounded-md p-2 font-medium ${className}`} ref={ref} placeholderTextColor="#6b7280" {...props} />
       </View>
     );
   }
 );
 
-SearchBar.displayName = "SearchBar";
-
-const styles = StyleSheet.create({
-  container: {
-    flexDirection: "row",
-    alignItems: "center",
-    gap: 2,
-    paddingHorizontal: 16,
-    paddingVertical: 8,
-    borderWidth: 1,
-    borderColor: "#e5e7ee",
-    borderRadius: 8,
-    maxWidth: 350,
-  },
-  input: {
-    flex: 1,
-    padding: 10,
-    borderWidth: 0,
-    fontSize: 16,
-  },
-});
 
 export default SearchBar;
--- client/screens/TestingScreen.tsx
+++ client/screens/TestingScreen.tsx
@@ -7,10 +7,11 @@ export default function App() {
   const [input, setInput] = useState("");
 
   return (
-    <View style={styles.container}>
+    <View className="flex-1 bg-white items-center justify-center">
       <Text>This is a testing page.</Text>
       {/* Add New Components here */}
       <SearchBar
+        className="mt-4 w-full"
         placeholder="searching.."
         value={input}
         onChangeText={(text) => setInput(text)}
@@ -19,11 +20,3 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
