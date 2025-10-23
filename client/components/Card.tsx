f1c650cda959c1f97ffc12803e4ad78425bff967
Jeremy Cortez <jeremycortez@dhcp-vl2041-45136.eduroam.cornell.edu>
2025-10-23 19:05:07 -0400

    implemented changes for Card-atom

--- client/components/Card.tsx
+++ client/components/Card.tsx
@@ -1,9 +1,6 @@
-import React, {FC} from "react";
+import React, { FC } from "react";
 import { View, Text } from "react-native";
 import { LucideIcon } from "lucide-react-native";
-
-
-
 
 type CardProps = {
   className?: string;
@@ -14,13 +11,13 @@ type CardProps = {
 
 const Card: FC<CardProps> = ({ className, icon: Icon, label, description }) => {
   return (
-    <View className={`bg-white border-2 border-gray-300 rounded-[10px] px-4 py-6 shadow-lg ${className}`}>
+    <View className={`bg-white border-gray-300 rounded-[8px] p-[20px] shadow-lg ${className}`}>
       <View className="flex-col mb-2 gap-4">
-        {Icon && <Icon size={24} color="#0F172A" className="mr-2" />}
-        <Text className="text-[24px] font-normal text-slate-900">{label}</Text>
+        {Icon && <Icon size={24} color="#0F172A"  />}
+        <Text className="text-[24px] font-normal text-slate-900 mt-[13px]">{label}</Text>
       </View >
       {description && (
-        <Text className="text-[18px] text-[#7C7C7C] leading-snug mt-2">{description}</Text>
+        <Text className="text-[18px] text-[#7C7C7C] leading-snug mt-[20px]">{description}</Text>
       )}
     </View>
   );
