cc2f31d67a479fa0b6a8b51adfcbd19d0a054f71
sanikashar <sanikasharma@gmail.com>
2025-11-13 12:40:10 -0500

    Revert "Implemented first two frames of about page"
    This reverts commit 9397643e25b5f0277ba1d2de77d9c8840a40fc96.

--- client/screens/TestingScreen.tsx
+++ client/screens/TestingScreen.tsx
@@ -1,37 +1,40 @@
-// import { Star } from "lucide-react-native";
+import { StyleSheet, Text, View } from "react-native";
 import { useState } from "react";
-import { View } from "react-native";
+import { Star } from "lucide-react-native";
 
-import About from "./about";
+import SearchBar from "../components/SearchBar";
+import Card from "../components/Card";
+import ProfileIcon from "../components/ProfileIcon";
+import Button from "../components/Button";
 
 export default function App() {
   const [input, setInput] = useState("");
 
   return (
     <View className="flex-1 bg-white items-center justify-center">
-      {/* <Text>This is a testing page.</Text> */}
-      < About/>
-      {/* <ProfileIcon
+      <Text>This is a testing page.</Text>
+      {/* Add New Components here */}
+      <ProfileIcon
         label="Monika Safford, MD"
         description="PALS Co-Founder"
-      /> */}
-      {/* <SearchBar
+      />
+      <SearchBar
         className="mt-4 w-full"
         placeholder="searching.."
         value={input}
         onChangeText={(text) => setInput(text)}
-      /> */}
+      />
 
-      {/* <Card
+      <Card
         icon={Star}
         label="Health Insights"
         description="View detailed analytics about your recent health data."
         className="w-[90%] mt-8 mb-8"
-      /> */}
-      {/* <Button
+      />
+      <Button
         label="Test Button"
         onPress={() => console.log("✅", "The button was pressed!")}
-      /> */}
+      />
     </View>
   );
 }
